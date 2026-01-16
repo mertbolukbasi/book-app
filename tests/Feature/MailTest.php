3d92e30900729b2718e8a4c90cdd03f28c955cba
mertbolukbasi <kaizenium@kaizeniumfoundation.com>
2026-01-16 18:48:42 +0300

    Mail and testing were completed

---
+++ tests/Feature/MailTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Mail\BookCreatedMail;
+use Illuminate\Support\Facades\Mail;
+use App\Models\Book;
+use Database\Factories\BookFactory;
+
+test('Test mail', function () {
+
+    Mail::fake();
+    $book = BookFactory::new()->create();
+    Mail::to('test@example.com')->send(new BookCreatedMail($book));
+    Mail::assertSent(BookCreatedMail::class);
+});
